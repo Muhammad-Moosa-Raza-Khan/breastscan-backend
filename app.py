59725de7f86f6098db2e9e83a0aa2c6c5a7f256f
Muhammad Moosa Raza Khan <razamoosa733@gmail.com>
2026-01-18 20:56:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -289,5 +289,5 @@ def health_check():
     return "Breast Cancer AI Backend Online", 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port)
